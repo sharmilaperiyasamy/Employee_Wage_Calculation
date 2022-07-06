ec3b7511145a697267c35732638dd037899749c6
sharmilaperiyasamy <sharmilaperiyasamy1992@gmail.com>
2022-07-06 14:54:42 +0530

    [Sharmila] Calculate Daily wage

--- Employee_wage/Employee_Details.cs
+++ Employee_wage/Employee_Details.cs
@@ -8,14 +8,18 @@ namespace Employee_wage
 {
     internal class Employee_Details
     {
+        int Full_Time = 1;
+        int wage_per_hour = 20;
         public void CheckAttendance()
         {
-            int Full_Time = 1;
             Random rand = new Random();
             int Attendance = rand.Next(0,2);
             if (Attendance == Full_Time)
             {
                 Console.WriteLine("Employee is Present");
+                int emp_hour = 8;
+                double wage = emp_hour * wage_per_hour;
+                Console.WriteLine("Daily wage of the Employee : " + wage);
             }
             else
             {
